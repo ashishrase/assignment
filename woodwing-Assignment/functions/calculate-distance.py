6be702862cbf0efacefe4f48fe552b23b0014a87
ashishrase <85737140+ashishrase@users.noreply.github.com>
2021-06-12 18:59:47 +0200

    Add files via upload

--- woodwing-Assignment/functions/calculate-distance.py
+++ woodwing-Assignment/functions/calculate-distance.py
@@ -1,38 +1,29 @@
 import json
 
-#Function definition to get distance value in over the REST service
+x=0
+y=0
+
 def distanceAddition(event, context):
     print(event)
-    distanceX=0
-    distanceY=0
-    m1 = "meters"
-    m2 = "m"
     http_method = event['httpMethod']
     
-    if http_method == 'GET':
-        distanceX = int(event['queryStringParameters']['distanceX'])
-        distanceY = int(event['queryStringParameters']['distanceY'])
-        units = (event['queryStringParameters']['units'])      
+    if http_method == 'GET' :
+        x = int(event['queryStringParameters']['x'])
+        y = int(event['queryStringParameters']['y'])
+        
     elif http_method == 'POST':
         body = json.loads(event['body'])
-        distanceX = body['distanceX']
-        distanceY = body['distanceX']
-        units = body['units']
-       
-        
-    if type(distanceX).__name__ != int or type(distanceY).__name__ != int :
-	    if units != m1:
-		    return("Currently Supporting only unit as meters")
-	    else:
-	       return {'statusCode': 200,'body': json.dumps(calculate(distanceX,distanceY,units))}
-    else:
-	    print("400 Invalid Operand")
-
-#calculate defined 
-def calculate(distanceX,distanceY,units):
-    result=0
-    result = distanceX + distanceY
-    finalresult = str(result) + ' ' + units
-    return finalresult
+        x = body['x']
+        y = body['y']
     
-  
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(calculate(x,y))
+   }
+
+def calculate(x,y):
+    result = 0
+    result = x + y
+    
+    return result
